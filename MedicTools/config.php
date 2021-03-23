99e03914eb1cd2d57c72d8e3cf6c4b61147f1189
dionis <dionis.rodrigo2@gmail.com>
2021-03-23 00:40:50 -0300

    Terminando a tela de usuario e medico

---
+++ MedicTools/config.php
@@ -0,0 +1,34 @@
+<?php 
+
+//DADOS PARA CONEXÃO COM BANCO DE DADOS LOCAL
+
+$host = 'localhost';
+$usuario = 'root';
+$senha = '';
+$banco = 'sysmedical';
+
+
+
+
+
+
+//VALORES PARA A COMBOBOX DE PAGINAÇÃO
+$opcao1 = 5;
+$opcao2 = 8;
+$opcao3 = 10;
+
+
+//VARIAVEL PARA DEFINIR O CAMINHO DO SISTEMA
+$url_sistema = 'http://sistemapdvteste-com-br.umbler.net';
+//$url_sistema = 'http://localhost/sysmedical';
+
+$email_adm = 'hugovasconcelosf@hotmail.com';
+
+$cidade = 'Belo Horizonte';
+
+
+$nivel_estoque = 5;
+$itens_tela = 6;
+$tempo_atualizacao_tela = 15;
+
+ ?>
