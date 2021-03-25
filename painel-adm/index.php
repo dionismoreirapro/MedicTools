908625fc991545459b0b7d3f80ce00d2e3837481
dionis <dionis.rodrigo2@gmail.com>
2021-03-25 00:38:32 -0300

    Incluindo tela de recepcionista

--- painel-adm/index.php
+++ painel-adm/index.php
@@ -62,7 +62,9 @@ if(@$_GET['acao'] == $item1){
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-	<link rel="stylesheet" href="../css/painel.css">
+	
+	<link  rel="stylesheet" href="../css/painel.css">
+	
 
 	<!--REFERENCIA PARA O FAVICON -->
 	<link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
